bb126e887c171921c62f6410aeaae7f75b0c294f
Purvi Arora <purvimps@gmail.com>
2026-07-04 19:28:16 +0530

    Delete day9 directory

--- day9/assests/js/counter.js
+++
@@ -1,9 +0,0 @@
-function plus(){
-    let n = parseInt( document.getElementById("num").innerText);
-    document.getElementById("num").innerText = n+1
-}
-function minus(){
-    let n = parseInt( document.getElementById("num").innerText);
-    document.getElementById("num").innerText = n-1
-
-}
--- day9/assests/js/digitalClock.js
+++
@@ -1,5 +0,0 @@
-setInterval(()=>{
-            let d = new Date();
-            document.getElementById("time").innerText = d.toLocaleTimeString();
-            console.log(d.toLocaleString())
-        }, 1000)
